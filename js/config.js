1f8ca19afcaeb34d7fb0db99110844c9082e4eeb
Imran-IND <ihussainmuhammad9@gmail.com>
2026-01-30 18:25:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,3 @@
 // Google Apps Script Web App URL
-const API_URL = "https://script.google.com/macros/s/AKfycbz4vIIbruYyArSppXl4LI_B821GhwkS8sLYW1Z0rGxRpJ6ia1FIg3vUUjs7Z0iS2AlP/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyg208SnPeuTxys5CE-B3bDWXHUKk_VPulKB_xfUcrJAXHiwAEuRFTPe7IYmpylcMb7/exec";
+
